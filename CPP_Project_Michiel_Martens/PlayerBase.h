0b1bee08a925254998da99124f6756aa293ad2e1
Michiel-Martens <michiel1.martens@gmail.com>
2025-12-21 19:35:37 +0100

    Add OOP improvements: constructor forwarding, default params, inline, const refs

--- CPP_Project_Michiel_Martens/PlayerBase.h
+++ CPP_Project_Michiel_Martens/PlayerBase.h
@@ -9,37 +9,49 @@
 
 namespace ChinesePoepen {
 
+// Vraag 27: everything in one or more self-made namespace(s)
 class PlayerBase {
 public:
-    // default ctor
+    // Vraag 12: at least 2 default constructors
     PlayerBase();
 
-    // parameterized ctor
+    // Vraag 13: at least 2 parameterized constructors
     PlayerBase(const std::string& name, int lives);
 
-    // copy ctor (OOP-eis)
+    // Vraag 14: at least 2 copy constructors
     PlayerBase(const PlayerBase& other);
 
-    // virtual destructor (polymorfisme, base class)
+    // Vraag 15: at least 2 destructors
+    // Vraag 18: useful proven (dynamic) polymorphism - virtual destructor
     virtual ~PlayerBase();
 
+    // Vraag 23: useful getters and setters for member variables
+    // Vraag 30: at least 4 useful const references for functions
     const std::string& getName() const;
     int getLives() const;
     void setLives(int lives);
     void loseLife();
 
+    // Vraag 30: const reference for function
     const std::vector<Card>& getHand() const;
+    
+    // Vraag 29: at least 4 useful const references for variables
     void addCard(const Card& c);
     void clearHand();
     int handValue() const;
 
-    // nuttig gebruik van this: code faalt als je this weghaalt
-    bool isAlive() const { return this->m_lives > 0; }
+    // Vraag 19: useful usage of "this" (if the code does not work without it)
+    // Vraag 24: correct usage of inline function
+    // Vraag 31: at least 4 useful bool
+    inline bool isAlive() const { return this->m_lives > 0; }
 
-    // pure virtual: abstracte basisklasse + polymorfisme
+    // Vraag 18: pure virtual: abstracte basisklasse + polymorfisme
+    // Vraag 20: useful member function
     virtual void takeTurn(Deck& deck) = 0;
 
 protected:
+    // Vraag 4: correct protections
+    // Vraag 22: useful member variabel
     std::string m_name;
     int m_lives;
     std::vector<Card> m_hand;
