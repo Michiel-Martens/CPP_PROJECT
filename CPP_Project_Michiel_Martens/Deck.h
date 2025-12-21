d7a9d031c45e1e4d3cacffa71be2beadf03fb8af
Michiel-Martens <michiel1.martens@gmail.com>
2025-12-21 19:37:33 +0100

    Add comments and improvements to Deck

--- CPP_Project_Michiel_Martens/Deck.h
+++ CPP_Project_Michiel_Martens/Deck.h
@@ -7,12 +7,20 @@
 
 class Deck {
 public:
+    // Vraag 12: default constructor
     Deck();
+    
+    // Vraag 20: useful member functions
     void shuffle();
     Card draw();
+    
+    // Vraag 30: const functions
+    // Vraag 31: useful bool
     bool empty() const;
     int size() const;
+    
 private:
+    // Vraag 36: useful container class
     std::vector<Card> m_cards;
 };
 
