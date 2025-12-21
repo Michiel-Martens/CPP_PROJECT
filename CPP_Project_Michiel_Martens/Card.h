f668df7fcb50a04eec74e8a906592da9c0187d44
Michiel-Martens <michiel1.martens@gmail.com>
2025-12-21 19:36:09 +0100

    Add template function and default params to Card

--- CPP_Project_Michiel_Martens/Card.h
+++ CPP_Project_Michiel_Martens/Card.h
@@ -13,24 +13,46 @@ enum class Rank {
 
 class Card {
 public:
+    // Vraag 12: default constructor
     Card();
+    
+    // Vraag 13: parameterized constructor
     Card(Rank rank, Suit suit);
 
+    // Vraag 23: getters
+    // Vraag 30: const function
     Rank getRank() const;
     Suit getSuit() const;
 
-    // Geeft een numerieke waarde die je kunt gebruiken voor scoring / vergelijkingen
+    // Vraag 20: useful member function
+    // Vraag 30: const function
     int value() const;
 
-    // Tekstweergave, bijvoorbeeld: "A♠" of "10♥"
+    // Vraag 35: useful string class usage
+    // Vraag 30: const function
     std::string toString() const;
 
-    // Voor makkelijk printen: std::cout << card;
+    // Vraag 26: useful friend function
     friend std::ostream& operator<<(std::ostream& os, const Card& card);
 
 private:
+    // Vraag 22: useful member variables
     Rank m_rank;
     Suit m_suit;
 };
+
+// Vraag 25: useful template function
+// Template functie om twee waardes te vergelijken op basis van een member functie
+template<typename T, typename Func>
+bool compareBy(const T& a, const T& b, Func getter) {
+    return getter(a) < getter(b);
+}
+
+// Vraag 25: Template specialization voorbeeld
+// Nuttig om kaarten te vergelijken op waarde
+template<typename T>
+T maxValue(const T& a, const T& b) {
+    return (a.value() > b.value()) ? a : b;
+}
 
 #endif // CARD_H
