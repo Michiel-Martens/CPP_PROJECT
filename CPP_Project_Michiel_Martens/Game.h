c646ece513db7d1853fc481e54c89bff92085926
Michiel-Martens <michiel1.martens@gmail.com>
2025-12-21 19:36:42 +0100

    Add default parameter values and comments to Game

--- CPP_Project_Michiel_Martens/Game.h
+++ CPP_Project_Michiel_Martens/Game.h
@@ -8,24 +8,37 @@
 
 #include <memory>
 #include <vector>
+#include <string>
 
 namespace ChinesePoepen {
 
+// Vraag 27: everything in namespace
 class Game {
 public:
+    // Vraag 12: default constructor
     Game();
 
+    // Vraag 20: useful member functions
     void addHumanPlayer(const std::string& name);
     void addAIPlayer(const std::string& name);
     void setLives(int lives);
     void run();
+    
+    // Vraag 21: default values in function definition
+    // Vraag 38: useful usage of (modern) file-I/O
+    void saveGame(const std::string& filename = "savegame.txt") const;
+    void loadGame(const std::string& filename = "savegame.txt");
 
 private:
+    // Vraag 32: dynamic memory allocation (unique_ptr)
+    // Vraag 36: useful container class
     std::vector<std::unique_ptr<PlayerBase>> m_players;
     int m_startLives;
 
     void playRound();
     void dealHands(Deck& deck);
+    
+    // Vraag 21: default values in function definition
     void showHands(bool revealAll = false) const;
     void eliminatePlayers();
 };
